403c9d6160eae74e849783cb444c6753f588eb3a
mukesh644009 <er.muks1988@gmail.com>
2022-03-04 23:56:45 +0530

    test

--- Priyanka/src/com/java/priyanka/Demo1.java
+++ Priyanka/src/com/java/priyanka/Demo1.java
@@ -1,9 +1,17 @@
 package com.java.priyanka;
 
+/* Git add .
+ * Git commit -m "message"
+ * Git push --set-upstream origin priyanka
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("test");
-		//
-		//
+	
 	}
 }
